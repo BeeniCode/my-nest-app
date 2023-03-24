aaeaf561498b8ee9e9907ac142a1dd86b1de3eb1
Beeni <beeni4429@gmail.com>
2023-03-24 17:18:02 +0500

    MongoURI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeeModule } from './employee.module';
-
+const MONGOURI: string =
+  'mongodb+srv://mubeen:xwMHMxCMVqhEqkRe@cluster0.zaasaak.mongodb.net/Employee';
+// const MONGOURI:string = "mongodb://localhost:27017/Employee"
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Employee'),
-    EmployeeModule,
-  ],
+  imports: [MongooseModule.forRoot(MONGOURI), EmployeeModule],
 })
 export class AppModule {}
